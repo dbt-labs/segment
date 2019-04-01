364cb812b69903136ce5b2bbc023f2ad8db6652a
Claire Carroll <carroll.claire.e@gmail.com>
2019-04-01 13:30:57 -0400

    Add extra fields to segment_web_user_stitching

--- macros/cross-adapter-modeling/sessionization/segment_web_user_stitching.sql
+++ macros/cross-adapter-modeling/sessionization/segment_web_user_stitching.sql
@@ -20,13 +20,21 @@ mapping as (
     select distinct
     
         anonymous_id, 
-        
+
         last_value(user_id ignore nulls) over (
             partition by anonymous_id 
             order by tstamp 
             rows between unbounded preceding and unbounded following
-            ) as user_id
-        
+        ) as user_id,
+
+        min(tstamp) over (
+            partition by anonymous_id
+        ) as first_seen_at,
+
+        max(tstamp) over (
+            partition by anonymous_id
+        ) as last_seen_at
+
     from events
 
 )
