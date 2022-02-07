782694b3fdf51d574ad851aecd7a319ba7a33074
Simon Podhajsky <simon.podhajsky@gmail.com>
2022-02-07 10:11:39 -0800

    Finish PG workaround for last_value(ignore nulls)
    Postgres doesn't have "ignore nulls", so instead we partition over
    anonymous_id and order the user_ids within it such that non-null
    values come up last, sorted secondarily by increasing tstamp.
    
    See 64b904d03b44f2f99d16b94bc08307d6872ed2a5 for first part;
    see https://stackoverflow.com/a/47223416/2114580 for solution.

--- models/sessionization/segment_web_user_stitching.sql
+++ models/sessionization/segment_web_user_stitching.sql
@@ -11,10 +11,20 @@ mapping as (
     select distinct
 
         anonymous_id,
+        
+        {# 
+            Postgres doesn't have "ignore nulls", so instead we partition over
+            anonymous_id and order the user_ids within it such that non-null
+            values come up last, sorted secondarily by increasing tstamp.
+        #}
 
         last_value(user_id {% if target.type != "postgres" -%} ignore nulls {%- endif -%}) over (
             partition by anonymous_id
-            order by tstamp
+            order by
+                {% if target.type == "postgres" -%}
+                case when user_id is not null then 1 else 0 end asc, 
+                {%- endif %}
+                tstamp
             rows between unbounded preceding and unbounded following
         ) as user_id,
 
