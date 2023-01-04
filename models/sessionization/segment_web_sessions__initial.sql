447f7d4499be07b14acb1aff493e13d1b54ee04f
0adamjones <105521832+0adamjones@users.noreply.github.com>
2023-01-04 13:02:29 -0800

    missing a comma

--- models/sessionization/segment_web_sessions__initial.sql
+++ models/sessionization/segment_web_sessions__initial.sql
@@ -21,7 +21,7 @@
     'utm_medium' : 'utm_medium',
     'utm_campaign' : 'utm_campaign',
     'utm_term' : 'utm_term',
-    'utm_type' : 'utm_type'
+    'utm_type' : 'utm_type',
     'gclid' : 'gclid',
     'page_url' : 'first_page_url',
     'page_url_host' : 'first_page_url_host',
