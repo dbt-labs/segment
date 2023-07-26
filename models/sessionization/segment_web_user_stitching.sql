6780deca7f7c923ebd1fd05f2788492096d569cc
lukersmithoscar <113738417+lukersmithoscar@users.noreply.github.com>
2023-07-26 12:40:37 +1000

    Push

--- models/sessionization/segment_web_user_stitching.sql
+++ models/sessionization/segment_web_user_stitching.sql
@@ -1,5 +1,39 @@
 {{config(materialized = 'table')}}
 
+with source as (
+
+    select * from {{ source('lyka_interface_prod', 'identifies') }}
+    where CHAR_LENGTH(user_id) = 5 --AL: last observed error rate of 8 (includes 'Checkout Completed') on 19 Jun 2023
+),
+
+renamed as (
+
+    select
+        distinct
+        anonymous_id,
+        user_id,
+        timestamp,
+        row_number() over (partition by anonymous_id order by timestamp desc) as sequence_number, --AL: sequence_number = 1 will be the most recent (timestamp) identify call on the user
+    from source
+
+), dataset as (
+
+    select
+        anonymous_id,
+        user_id,
+        row_number() over (partition by user_id order by timestamp desc) as device_sequence_number --AL: device_sequence_number = 1 will be the most recent (timestamp) device that had an identify call
+    --AL: ast at 29/06/23 still very few instances where multiple annon_id mapped to single user_id.
+    from renamed
+    where 
+        sequence_number = 1
+)
+
+select *
+from
+    dataset
+where
+    device_sequence_number = 1 
+/*
 with events as (
 
     select * from {{ref('segment_web_page_views')}}
@@ -41,3 +75,4 @@ mapping as (
 )
 
 select * from mapping
+*/
