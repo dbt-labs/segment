c47d723db31613a71476f2fd38c6d412b95d89ae
alewinskylyka <andrew.lewinsky@lyka.com.au>
2023-08-07 16:08:53 +0700

    Update segment_web_user_stitching.sql

--- models/sessionization/segment_web_user_stitching.sql
+++ models/sessionization/segment_web_user_stitching.sql
@@ -1,28 +1,39 @@
 {{config(materialized = 'table')}}
---hello
+
 with source as (
 
     select * from {{ source('lyka_interface_prod', 'identifies') }}
-    where CHAR_LENGTH(user_id) = 5 --AL: last observed error rate of 8 (includes 'Checkout Completed') on 19 Jun 2023
-),
+    where email is not null
+    or (user_id is not null and CHAR_LENGTH(user_id) = 5) --AL: last observed error rate of 8 (includes 'Checkout Completed') on 19 Jun 2023
+)
 
-renamed as (
+--AL: sequence_number = 1 will be the most recent (timestamp) identify call on the user
+
+
+, email as (
+    select
+        distinct
+        email,
+        user_id,
+    from source
+    where user_id is not null
+
+)
+
+, device as (
 
     select
         distinct
         anonymous_id,
+        email,
         user_id,
         timestamp,
         row_number() over (partition by anonymous_id order by timestamp desc) as sequence_number, --AL: sequence_number = 1 will be the most recent (timestamp) identify call on the user
     from source
-
 )
 
 select
-    anonymous_id,
-    user_id,
-    row_number() over (partition by user_id order by timestamp desc) as device_sequence_number --AL: device_sequence_number = 1 will be the most recent (timestamp) device that had an identify call
---AL: ast at 29/06/23 still very few instances where multiple annon_id mapped to single user_id.
-from renamed
-where 
-    sequence_number = 1
+device.anonymous_id, device.email, email.user_id
+from device
+left join email on device.email = email.email
+where device.sequence_number = 1
