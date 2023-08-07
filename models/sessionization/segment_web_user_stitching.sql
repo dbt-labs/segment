96b843d3603cf69a8b8f75271836129c34f5427a
alewinskylyka <andrew.lewinsky@lyka.com.au>
2023-08-07 17:18:01 +0700

    Update segment_web_user_stitching.sql

--- models/sessionization/segment_web_user_stitching.sql
+++ models/sessionization/segment_web_user_stitching.sql
@@ -13,7 +13,7 @@ with source as (
         distinct
         email,
         user_id,
-        row_number() over (partition by email order by timestamp desc) as sequence_number, --AL: sequence_number = 1 will be the most recent (timestamp) identify call on the user
+        row_number() over (partition by email order by timestamp desc) as sequence_number, --AL: sequence_number = 1 will be the most recent (timestamp) identify call against the user
     from source
     where user_id is not null
 )
@@ -25,12 +25,12 @@ with source as (
         anonymous_id,
         email,
         user_id,
-        row_number() over (partition by anonymous_id order by timestamp desc) as sequence_number, --AL: sequence_number = 1 will be the most recent (timestamp) identify call on the user
+        row_number() over (partition by anonymous_id order by timestamp desc) as sequence_number, --AL: sequence_number = 1 will be the most recent (timestamp) identify call against the email
     from source
 )
 
 select
 device.anonymous_id, device.email, identify.user_id
 from device
-left join identify on device.email = identify.email and identify.sequence number = 1
+left join identify on device.email = identify.email and identify.sequence_number = 1
 where device.sequence_number = 1
