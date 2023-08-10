27cca5ba82af14082a885b8a68430d76597debcd
alewinskylyka <andrew.lewinsky@lyka.com.au>
2023-08-10 11:39:11 +0700

    Update segment_web_user_stitching.sql

--- models/sessionization/segment_web_user_stitching.sql
+++ models/sessionization/segment_web_user_stitching.sql
@@ -1,7 +1,10 @@
 with source as (
 
-    select * from {{ source('lyka_interface_prod', 'identifies') }}
+    select
+        anonymous_id, email, user_id, timestamp
+    from {{ source('lyka_interface_prod', 'identifies') }}
     where (email is not null or user_id is not null)
+    order by timestamp
 )
 
 , identify as (
@@ -9,7 +12,7 @@ with source as (
         distinct
         email,
         user_id,
-        row_number() over (partition by email order by timestamp desc) as sequence_number, --AL: sequence_number = 1 will be the most recent (timestamp) identify call against the user
+        row_number() over (partition by email order by timestamp desc) as sequence_number, --AL: sequence_number = 1 will be where the last time a unique email has been associated with a Lyka User ID
     from source
     where user_id is not null
 )
@@ -20,13 +23,12 @@ with source as (
         distinct
         anonymous_id,
         email,
-        user_id,
-        row_number() over (partition by anonymous_id order by timestamp desc) as sequence_number, --AL: sequence_number = 1 will be the most recent (timestamp) identify call against the email
+        row_number() over (partition by anonymous_id order by timestamp desc) as sequence_number, --AL: sequence_number = 1 will be where the last time a unique anonymous ID has been associated with an email
     from source
 )
 
 select
-device.anonymous_id, device.email, identify.user_id,
+device.anonymous_id, device.email, identify.user_id --AL: if an email is associated with a Lyka User ID then it will be stitched onto the anonymous_id
 from device
 left join identify on device.email = identify.email and identify.sequence_number = 1
 where device.sequence_number = 1
