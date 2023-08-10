c1c5fc6e2108b9ca23e13992d09bdb5ca559917e
alewinskylyka <andrew.lewinsky@lyka.com.au>
2023-08-10 10:25:02 +0700

    Update segment_web_user_stitching.sql

--- models/sessionization/segment_web_user_stitching.sql
+++ models/sessionization/segment_web_user_stitching.sql
@@ -21,12 +21,19 @@ with source as (
         anonymous_id,
         email,
         user_id,
+        timestamp,
         row_number() over (partition by anonymous_id order by timestamp desc) as sequence_number, --AL: sequence_number = 1 will be the most recent (timestamp) identify call against the email
     from source
 )
 
 select
 device.anonymous_id, device.email, identify.user_id,
+case
+    when device.user_id is not null
+    then row_number() over (partition by device.user_id order by device.timestamp asc)
+    else null
+end as user_id_identify_sequence_asc
+    --AL: this is to ensure we are using the first identify call against a Lyka UID to tie to an anonymous ID given checkout complete event does not capture anonymouse ID
 from device
 left join identify on device.email = identify.email and identify.sequence_number = 1
 where device.sequence_number = 1
