333e18a7b697d924a01657bcbdf0e7e51842236e
alewinskylyka <andrew.lewinsky@lyka.com.au>
2023-08-15 17:25:29 +1000

    final

--- models/sessionization/segment_web_user_stitching.sql
+++ models/sessionization/segment_web_user_stitching.sql
@@ -1,30 +1,45 @@
 with source as (
+
     select
         anonymous_id, email, user_id, timestamp
     from {{ source('lyka_interface_prod', 'identifies') }}
-    where (email is not null and anonymous_id is not null) or (user_id is not null and anonymous_id is not null)
-    order by timestamp
+    where email is not null and (user_id is not null or anonymous_id is not null)
+
 )
 
 , identify as (
+
+--Each email can be associated with only 1 user, however a user can have many emails
+
     select
         distinct
         email,
         user_id,
-        row_number() over (partition by email order by timestamp desc) as sequence_number, --AL: sequence_number = 1 will be where the last time a unique email has been associated with a Lyka User ID
+        row_number() over (partition by email order by timestamp desc) as sequence_number
     from source
-    where user_id is not null
+    where email is not null and user_id is not null
+
 )
+
 , device as (
+
+--Each email can be associated with only 1 id, however an id can have many emails
+
     select
         distinct
-        anonymous_id,
         email,
-        row_number() over (partition by anonymous_id order by timestamp desc) as sequence_number, --AL: sequence_number = 1 will be where the last time a unique anonymous ID has been associated with an email
+        anonymous_id,
+        row_number() over (partition by email order by timestamp desc) as sequence_number
     from source
+    where email is not null and anonymous_id is not null
 )
+
+--For the final results, take the user ID if known, and/or email otherwise
+
 select
-device.anonymous_id, device.email, identify.user_id --AL: if an email is associated with a Lyka User ID then it will be stitched onto the anonymous_id
-from device
-left join identify on device.email = identify.email and identify.sequence_number = 1
-where device.sequence_number = 1
+identify.user_id, identify.email, device.anonymous_id --AL: if an email is associated with a Lyka User ID then it will be stitched onto the anonymous_id
+from identify
+left join device on identify.email = device.email and device.sequence_number = 1
+where identify.sequence_number = 1
+
+--For validation, ensure each id is only represented once for each email and user (i.e. unique) and that email is only associated with one user (but can have many ids)
