2804c86d7b61919bfd01b2763948905c4d8746c5
alewinskylyka <andrew.lewinsky@lyka.com.au>
2023-08-22 15:13:40 +1000

    Update segment_web_user_stitching.sql

--- models/sessionization/segment_web_user_stitching.sql
+++ models/sessionization/segment_web_user_stitching.sql
@@ -27,12 +27,12 @@ with source as (
 
     select
         distinct
-        {# anonymous_id, #}
+        anonymous_id,
         email,
         user_id,
         row_number() over (partition by email order by timestamp desc) as sequence_number
     from source
-    where email is not null and user_id is not null
+    where anonymous_id is not null and email is not null and user_id is not null
 
 )
 
@@ -63,7 +63,7 @@ with source as (
         email,
         row_number() over (partition by anonymous_id order by timestamp desc) as sequence_number
     from source
-    where anonymous_id is not null and email is not null
+    where anonymous_id is not null and email is not null and user_id is null
 
 )
 
@@ -82,4 +82,4 @@ end as user_identified_rank
 from anonymous_id
 left join unknown_email on anonymous_id.anonymous_id = unknown_email.anonymous_id and unknown_email.sequence_number = 1
 left join known_user on anonymous_id.anonymous_id = known_user.anonymous_id and known_user.sequence_number = 1
-left join known_email on unknown_email.email = known_email.email and known_email.sequence_number = 1
+left join known_email on anonymous_id.anonymous_id = known_email.anonymous_id and known_email.sequence_number = 1
